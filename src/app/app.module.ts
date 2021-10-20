c94ca2fe0d44b3b6b1fa5f537ad335c5ccefec2c
Jeremy Hastings <jeremy.hastings@mac.com>
2021-10-20 10:10:40 -0500

    Create Documents Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DocumentsComponent } from './documents/documents.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DocumentsComponent,
     HomepageComponent
   ],
   imports: [
