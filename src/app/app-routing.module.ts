3a0beb07b6b673be390058606b4d2f540d527937
Jeremy Hastings <jeremy.hastings@mac.com>
2021-10-20 11:21:23 -0500

    Creating Initial Proposal Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DocumentsComponent } from './documents/documents.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'documents', component: DocumentsComponent },
-  { path: 'home', component: HomepageComponent }
+  { path: 'home', component: HomepageComponent },
+  { path: 'proposals', component: ProposalListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DocumentsComponent,
-    HomepageComponent
+    HomepageComponent,
+    ProposalListComponent
   ],
   imports: [
     BrowserModule,
