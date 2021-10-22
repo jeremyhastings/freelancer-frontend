89cddd7acb5c1057e5f018ed30879d5a00670886
Jeremy Hastings <jeremy.hastings@mac.com>
2021-10-22 11:09:37 -0500

    Using Bootstrap Cards

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
       description: 'This is dummy data until we connect to the API',
       file_url: 'http://google.com',
       updated_at: 'October 20th, 2021',
-      image_url: 'http://google.com'
+      image_url: 'https://i2.wp.com/sendtask.io/wp-content/uploads/2017/12/freelance-websites.jpg?w=2280&ssl=1'
     },
     {
       title: 'My Second Document',
       description: 'This is dummy data until we connect to the API',
       file_url: 'http://google.com',
       updated_at: 'October 20th, 2021',
-      image_url: 'http://google.com'
+      image_url: 'https://i2.wp.com/sendtask.io/wp-content/uploads/2017/12/freelance-websites.jpg?w=2280&ssl=1'
     },
     {
       title: 'My Last Document',
       description: 'This is dummy data until we connect to the API',
       file_url: 'http://google.com',
       updated_at: 'October 20th, 2021',
-      image_url: 'http://google.com'
+      image_url: 'https://i2.wp.com/sendtask.io/wp-content/uploads/2017/12/freelance-websites.jpg?w=2280&ssl=1'
     }
   ]
 }
